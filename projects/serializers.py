5e91832929cf773ececa3494a4bf65ae02f5a3a3
Kacper <k.g.szmyd@gmail.com>
2025-05-14 19:44:11 +0200

    Add User, Task and Project serializers

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,3 +1,28 @@
-from rest_framework.serializers import Serializer
+from rest_framework import serializers
 from .models import Project, Task
 from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+        
+        
+
+class TaskSerializer(serializers.Serializer):
+    assigned_to = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'due_date', 'created_at', 'assignedd_to']
+        
+        
+
+class ProjectSerializer(serializers.Serializer):
+    members = UserSerializer(many=True, read_only=True)
+    tasks = TaskSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'created_at', 'members', 'tasks']
